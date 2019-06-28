b45584c897e4c686c82b76cd33dcf5dfee4540b9
Josh Waterhouse <joshua_waterhouse@hotmail.com>
2019-06-28 14:47:18 +1000

    Below level kill box script

---
+++ OutofTime/Assets/Resources/Scripts/Level/KillBox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillBox : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider trigger)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
